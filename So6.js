c29af909e70385d51cdbc4698dedd465df3a37cf
ahmada <ahmadakbarpoornjad@gmail.com>
2023-11-07 16:09:53 +0330

    addS06

---
+++ So6.js
@@ -0,0 +1,18 @@
+let http=require('http');
+let server =http.createServer(requestHandler);
+server.listen(8037);
+
+console.log('test');
+let headers={'Content-Type':'Text/Plain'};
+
+function requestHandler (request, response){
+    
+    console.log('request url: ', request.url);
+    console.log('request method: ', request.method);
+
+
+    response.writeHead(200,headers);
+    response.write('salam '+request.url);
+    response.end();
+
+}
